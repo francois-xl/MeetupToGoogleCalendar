05694b50e72aa3a265c6f217771f14c9ff3e5bdb
Francois-Xavier LAHITTE <francoisxavier.lahitte@gmail.com>
2015-07-28 23:40:20 +0100

    Added public API method 'events' and basic tests

--- lib/MeetupToGoogleCalendar.rb
+++ lib/MeetupToGoogleCalendar.rb
@@ -6,10 +6,25 @@ module MeetupToGoogleCalendar
     def initialize(meetup_group_url, api_key)
       @meetup_group_url = meetup_group_url
       @api_key = api_key
+      event_list
+      
     end
     
     def events     
-      []
+      event_list.each do |result|
+        # Do something with the result
+      end 
+      event_list
+    end
+    
+    private
+    
+    def get_events
+      client = RMeetup::Client.new do |config|
+        config.api_key = @api_key
+      end
+    
+      event_list = client.fetch(:events, { event_id: 'some_id' })      
     end
   end
 end
