39bc369cd261056f18159d5807fe00e539c834f4
Francois-Xavier LAHITTE <francoisxavier.lahitte@gmail.com>
2015-07-29 18:56:58 +0100

    Minimalist Functional version, copy 3 upcoming meetup to the authorized google calendar using a json file as credential

--- lib/MeetupToGoogleCalendar.rb
+++ lib/MeetupToGoogleCalendar.rb
@@ -1,30 +1,61 @@
 require "MeetupToGoogleCalendar/version"
 require "rmeetup"
+require "GoogleCalendarApiWrapper/googlecalendarapiwrapper"
 
 module MeetupToGoogleCalendar
   class TransfertControler
-    def initialize(meetup_group_url, api_key)
-      @meetup_group_url = meetup_group_url
+    def initialize(meetup_group_urlname, api_key, calendarid)
+      @meetup_group_urlname = meetup_group_urlname
       @api_key = api_key
-      event_list
-      
+      @googlewrapper = GoogleCalendarApiWrapper::GoogleCalendarApiWrapper.new(calendarid)
     end
-    
-    def events     
-      event_list.each do |result|
-        # Do something with the result
-      end 
-      event_list
+
+    ##
+    # TODO:
+    #
+    # @return the list of event for the given meetup group
+    def events
+      get_events
+      @event_list
     end
-    
+
+    ##
+    # Push all the retreive events to the given google calendar
+    #
+    def synchronize
+      get_events unless @event_list
+
+      puts "Count : " + @event_list.count.to_s
+      @event_list.each do |event|
+        #puts "Event : " + event.inspect
+        #event.duration = 10800000;
+        @googlewrapper.add_event(event.name, event.time, event.time + Rational(3 * 60 * 60, 24* 60 * 60), event.timezone)
+        puts "Ajouté !"
+      # TODO: Warning, event.duration may sometimes not be set, will it be nil ? or 0 ? will the addition work ? need to test this
+      end
+    end
+
     private
-    
+
+    ##
+    # Verify that the application have authorization to access the google calendar
+    #
+    def verify_google_authorization
+      #TODO:
+    end
+
+    ##
+    # Retreive and store the upcoming meetup event list from the given meetup group using the meetup API
+    #
     def get_events
       client = RMeetup::Client.new do |config|
         config.api_key = @api_key
       end
-    
-      event_list = client.fetch(:events, { event_id: 'some_id' })      
+
+      @event_list = client.fetch(:events, { :group_urlname => @meetup_group_urlname,
+                                            :status => 'upcoming',
+                                            :fields => 'timezone',
+                                            :page => 3 })
     end
   end
 end
--- spec/meetuptogooglecalendar/transfertcontroler_spec.rb
+++ spec/meetuptogooglecalendar/transfertcontroler_spec.rb
@@ -3,23 +3,24 @@ require 'spec_helper'
 module MeetupToGoogleCalendar
   describe TransfertControler do
     before do
-     @transfert_controler = MeetupToGoogleCalendar::TransfertControler.new("http://www.meetup.com/GeekZoneLondon/")
+      meetup_group_urlname = "GeekZoneLondon"
+      api_key = "457814a4b4336503971717b681f3f7b"
+      calendar_id =  "tr35eq66n1hlp8eg35avto1350@group.calendar.google.com"
+      @transfert_controler = MeetupToGoogleCalendar::TransfertControler.new(meetup_group_urlname, api_key, calendar_id)
     end
-      
+
     describe "#events" do
       it "returns the list of public meetup events" do
-        #expect(@transfert_controler.events).not_to be nil
+        expect(@transfert_controler.events).not_to be nil
         expect(@transfert_controler.events).to be_a(Array)
       end
     end
 
-=begin
     describe "#synchronize" do
-      it "returns the http status of the page" do
-        expect(@transfert_controler.status).to eql("200 OK")
+      it "add the event to the google calendar" do
+      @transfert_controler.synchronize
       end
     end
-=end
-    
+
   end
 end
